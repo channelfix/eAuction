08cefe7e6ba049ed6bd774d72495f1e7de58761d
bojoluis21798 <trinity2198@gmail.com>
2018-05-01 15:11:36 +0800

    Add title

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,4 +1,16 @@
 <template>
+	<v-container fluid>
+		<v-layout row wrap>
+		  	<v-flex lg7>
+		  		<v-layout justify-space-around row class="box intro">
+					<span class="title">eAuction</span>
+		  		</v-layout>
+		  	</v-flex>
+		  	<v-flex lg5>
+		  	 	
+		  	</v-flex>
+		</v-layout>
+	</v-container>
 </template>
 
 <script>
@@ -24,15 +36,10 @@
 </script>
 
 <style scoped>
+	.intro {
+		padding: 10em;
+	}
 	.title {
-		font-size: 5em;
-	}
-	.loginbox {
-		margin: 5%;
-		margin-top: 7%;
-		padding: 5%;
-	}
-	.desc {
-		text-align: center;
+		font-size: 20em;
 	}
 </style>
