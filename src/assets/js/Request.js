1dcaebfdbecbb93e3579648013797d90c031ab92
bojoluis21798 <trinity2198@gmail.com>
2018-05-07 10:22:52 +0800

    Create request class to handle api calls to backend

---
+++ src/assets/js/Request.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
+export default class Request {
+	send(method, baseurl, url, data){
+		axios({
+			url: url,
+			method: method,
+			baseURL: baseurl,
+			headers: {
+				'X-CSRFToken': Cookies.get('csrftoken')
+			},
+			data: data,
+		})
+		.then((response)=>{return response})
+		.catch((error)=>{return error})
+	}
+
+	post(baseurl, url, data){
+		send('post',baseurl, url, data);
+	}
+
+	get(baseurl, url, data){
+		send('get',baseurl, url, data);
+	}
+}
