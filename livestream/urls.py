117d1332f910bada787c222abdb98377cac5cae6
Gavin <gavincanete@gmail.com>
2018-05-09 18:34:44 +0800

    Rearranging the format

--- livestream/urls.py
+++ livestream/urls.py
@@ -4,9 +4,14 @@ from livestream.views import AuctioneerView, BidderView
 
 app_name = 'livestream'
 urlpatterns = [
-	path('', views.index, name = 'home'), # Location for main page
-	path('auctioneer/', AuctioneerView.as_view(), name = 'auctioneer'), # Location for auctioneer page.
-	path('bidder/', BidderView.as_view(), name = 'bidder'), # Location for bidder page.
-	path('start_archive/', views.start_archive, name = 'start_archive'), # Location for start recording the video.
-	path('end_archive/', views.end_archive, name = 'end_archive'), # Location for end recording the video.
+    # Location for main page
+    path('', views.index, name='home'),
+    # Location for auctioneer page.
+    path('auctioneer/', AuctioneerView.as_view(), name='auctioneer'),
+    # Location for bidder page.
+    path('bidder/', BidderView.as_view(), name='bidder'),
+    # Location for start recording the video.
+    path('start_archive/', views.start_archive, name='start_archive'),
+    # Location for end recording the video.
+    path('end_archive/', views.end_archive, name='end_archive'),
 ]
