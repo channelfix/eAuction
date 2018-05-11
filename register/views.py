c846b25bc87b0d29bdb235e66c6dbbf2c42d0f26
Zairiel Sarausad <zairiel243@gmail.com>
2018-05-11 21:39:18 +0800

    Add Running Back-end and JS Script

--- register/views.py
+++ register/views.py
@@ -3,26 +3,25 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.generic import View
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
+from django.db.utils import IntegrityError
 # Create your views here.
 
 
 class RegisterViewPost(View):
     def post(self, request):
-        firstname = request.POST.get('firstName')
-        lastname = request.POST.get('lastName')
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        user = User.objects.create_user(first_name=firstname,
-                                        last_name=lastname,
-                                        username=username,
-                                        email=email,
-                                        password=password,
-                                        is_staff=False,
-                                        is_admin=False)
-
-        if user.username_validator() is True:
-            return HttpResponse(json.dumps({'success': True}))
-        else:
+        try:
+            user = User.objects.create_user(first_name=firstname,
+                                            last_name=lastname,
+                                            username=username,
+                                            email=email,
+                                            password=password,)
+        except IntegrityError:
             return HttpResponseBadRequest()
+
+        return HttpResponse(json.dumps({'success': True}))
