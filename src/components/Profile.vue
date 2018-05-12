b4833bd2d4c0f5590522fd9e076f2a3cc124f54b
Gavin <gavincanete@gmail.com>
2018-05-12 19:45:11 +0800

    Add Profile.vue inorder to create profile page

---
+++ src/components/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+ 	<div id="profile_id"> 	
+ 		<button @click="requestProfile">request profile</button>
+ 		<p v-text="simpleText"></p>
+ 	</div>
+</template>
+
+<script type="text/javascript">
+	import axios from 'axios';
+
+	export default {
+		data() {
+			return {
+				simpleText: 'Livestream'
+			}
+		},
+		methods: {
+			requestProfile: function() {
+				axios.get("http://localhost:8000/profile/request_profile/")
+				.then((response) => {
+					// Requested Successfully
+					this.simpleText = response.data;
+				}, (error) => {
+					alert('Failed to request')
+				})
+			}
+		}
+	}
+
+	
+</script>
