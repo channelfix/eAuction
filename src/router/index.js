b1a2671e2842e695d09b9a48c7555e2cce2ffd2f
Gavin <gavincanete@gmail.com>
2018-05-12 19:46:32 +0800

    changing the url pattern for profile page to be access

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import LogIn from '@/components/LogIn'
+import Profile from '@/components/Profile'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'LogIn',
-      component: LogIn
+      name: 'Profile',
+      component: Profile
     }
   ]
 })
