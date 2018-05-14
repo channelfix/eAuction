843596ab393052c0b36ff3bfa0a275eae9c53ec4
Gavin <gavincanete@gmail.com>
2018-05-14 21:09:50 +0800

    remove userdefined user models

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from profiles.models import User, Profile
+from profiles.models import Profile
 from app.tags.models import Tags
 
-admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Tags)
-
 # Register your models here.
