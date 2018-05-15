f1c7f4562258a410feadb56b462f17c7fb485ad5
Gavin <gavincanete@gmail.com>
2018-05-15 19:55:49 +0800

    Converting queryset to dict

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import auth
+from django.contrib.auth.models import User
+from django.forms.models import model_to_dict
+from profiles.models import Profile
 import json
-from django.core import serializers
 
 
 def view_profile(request):
@@ -26,17 +28,40 @@ def request_profile(request):
 
     user = request.user
     if user and user.is_active:
+        # get the name, email from given User model.
+        queryset = User.objects.filter(username=user.username)
+        user_dict = list(queryset.values('first_name', 'last_name', 'email'))
+
+        # Get the Profile model
         user_profile = user.profile
-        user_tags = user_profile.tags_set.all()
 
-        serializer = serializers.serialize('json', [user, user_profile, user_tags])
+        # Get the filename for avatar
+        pic_name = user_profile.file_name
 
-        # context = {
-        #     'user': user,
-        #     'user_profile': user_profile,
-        #     'user_tags': user_tags
-        # }
+        # Get the Avatar model
+        avatar = user_profile.avatar
 
-        return HttpResponse(json.dumps(serializer))
+        # Get the Biography from a given Profile object
+        bio = user_profile.biography
+
+        context = {
+            'pic_name': pic_name,
+            'avatar': avatar,
+            'bio': bio
+        }
+
+        # get all the tags for this user instance.
+        # tags_dict = list(user.profile.tags_set.all().values())
+
+        # Concatenate all the fields given from
+        # User, Profile and Tag above.profile
+
+        # context.update(user_dict[0])
+        # context.update(profile_dict)
+        # context.update({'tags': tags_dict})
+
+        # print(context)
+
+        return HttpResponse(json.dumps(context))
 
     return HttpResponse('Failed to login')
