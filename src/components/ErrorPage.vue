dba056c4854221b01d20757ef3c6882e2486f31b
bojoluis21798 <trinity2198@gmail.com>
2018-05-16 11:48:25 +0800

    Created 404 Error page if accessing a page that does not exist

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-container 
+		fluid
+	>
+		<v-layout 
+			row 
+			wrap
+			fill-height
+		>
+	  		<v-flex>
+	  			<v-layout 
+	  				align-center
+					justify-center
+					fill-height
+  				>
+		  		 	<div>
+						<p class="text-lg-center display-4">404</p>
+						<p class="text-lg-center headline">The page you're looking for doesn't exist </3 </p>
+					</div>
+				</v-layout>
+	  		</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+<script>
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import LogIn from '@/components/LogIn'
 import Home from '@/components/Home'
+import ErrorPage from '@/components/ErrorPage'
 
 Vue.use(Router)
 
@@ -12,10 +13,16 @@ export default new Router({
 	    path: '',
         name: 'LogIn',
         component: LogIn
-    },{
+    },
+    {
     	path: '/home',
     	name: 'Home',
     	component: Home,
+    },
+    {
+      path: '*',
+      name: '404',
+      component: ErrorPage,
     }
   ]
 })
