6740f3c6a2739a9d2f7295883bb49ed4e8676968
bojoluis21798 <trinity2198@gmail.com>
2018-05-18 09:35:47 +0800

    Use for loop to render thumbnails

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,162 +1,63 @@
 <template>
-	<v-layout 
-		fill-height
-	>
-		<v-container 
-			fluid
+ 	<v-container
+		fluid
+		pa-5
 		>
-		  	<v-toolbar 
-	  			color="amber darken-3"
+		<v-layout
+			justify-center
+			row
+			wrap
+			v-if="livestreams.length == 0"
+		> <!-- if no content -->
+			<v-icon
+				large
+	 	   >
+	    		subscriptions
+			</v-icon>
+	  		<span class="headline">No subscriptions</span>
+		</v-layout>
+ 		<v-layout 
+ 			v-if="livestreams.length != 0"
+	 		row
+	 		wrap
 			>
-			    <v-toolbar-side-icon 
-			    	@click="showNav = !showNav"
-		   		>
-		   		</v-toolbar-side-icon>
-			    <v-toolbar-title>Home</v-toolbar-title>
-			    <v-spacer></v-spacer>
-		 	</v-toolbar>
-		 	<v-layout 
-		 		row
-		 		wrap
-				justify-space-around
-				mt-5
-	 		>
-		 		<v-flex md3>
-					<v-card color="grey darken-1">
-						<v-card-media src="http://lheventures.com/wp-content/uploads/sites/7/2017/10/Cooke-Auction-Property-pic.png" height="200px">
-						</v-card-media>	
-						<v-card-title primary-title>
-				          <div>
-				            <p class="headline mb-0">eAuction eAuction</p>
-				            <div>eAuction eAuction eAuction eAuction eAuction<br> eAuction eAuction eAuction eAuction</div>
-				          </div>
-				        </v-card-title>
-					</v-card>
-		 		</v-flex>
-		 		<v-flex md3>
-					<v-card color="grey darken-1">
-						<v-card-media src="http://lheventures.com/wp-content/uploads/sites/7/2017/10/Cooke-Auction-Property-pic.png" height="200px">
-						</v-card-media>	
-						<v-card-title primary-title>
-				          <div>
-				            <p class="headline mb-0">eAuction eAuction</p>
-				            <div>eAuction eAuction eAuction eAuction eAuction<br> eAuction eAuction eAuction eAuction</div>
-				          </div>
-				        </v-card-title>
-					</v-card>
-		 		</v-flex>
-		 		<v-flex md3>
-					<v-card color="grey darken-1">
-						<v-card-media src="http://lheventures.com/wp-content/uploads/sites/7/2017/10/Cooke-Auction-Property-pic.png" height="200px">
-						</v-card-media>	
-						<v-card-title primary-title>
-				          <div>
-				            <p class="headline mb-0">eAuction eAuction</p>
-				            <div>eAuction eAuction eAuction eAuction eAuction<br> eAuction eAuction eAuction eAuction</div>
-				          </div>
-				        </v-card-title>
-					</v-card>
-		 		</v-flex>
-		 	</v-layout>
-		 	<v-layout 
-		 		row
-		 		wrap
-				justify-space-around
-				mt-5
-	 		>
-		 		<v-flex md3>
-					<v-card color="grey darken-1">
-						<v-card-media src="http://lheventures.com/wp-content/uploads/sites/7/2017/10/Cooke-Auction-Property-pic.png" height="200px">
-						</v-card-media>	
-						<v-card-title primary-title>
-				          <div>
-				            <p class="headline mb-0">eAuction eAuction</p>
-				            <div>eAuction eAuction eAuction eAuction eAuction<br> eAuction eAuction eAuction eAuction</div>
-				          </div>
-				        </v-card-title>
-					</v-card>
-		 		</v-flex>
-		 		<v-flex md3>
-					<v-card color="grey darken-1">
-						<v-card-media src="http://lheventures.com/wp-content/uploads/sites/7/2017/10/Cooke-Auction-Property-pic.png" height="200px">
-						</v-card-media>	
-						<v-card-title primary-title>
-				          <div>
-				            <p class="headline mb-0">eAuction eAuction</p>
-				            <div>eAuction eAuction eAuction eAuction eAuction<br> eAuction eAuction eAuction eAuction</div>
-				          </div>
-				        </v-card-title>
-					</v-card>
-		 		</v-flex>
-		 		<v-flex md3>
-					<v-card color="grey darken-1">
-						<v-card-media src="http://lheventures.com/wp-content/uploads/sites/7/2017/10/Cooke-Auction-Property-pic.png" height="200px">
-						</v-card-media>	
-						<v-card-title primary-title>
-				          <div>
-				            <p class="headline mb-0">eAuction eAuction</p>
-				            <div>eAuction eAuction eAuction eAuction eAuction<br> eAuction eAuction eAuction eAuction</div>
-				          </div>
-				        </v-card-title>
-					</v-card>
-		 		</v-flex>
-		 	</v-layout>
-		 	<v-layout 
-		 		row
-		 		wrap
-				justify-space-around
-				mt-5
-				mb-5
-	 		>
-		 		<v-flex md3>
-					<v-card color="grey darken-1">
-						<v-card-media src="http://lheventures.com/wp-content/uploads/sites/7/2017/10/Cooke-Auction-Property-pic.png" height="200px">
-						</v-card-media>	
-						<v-card-title primary-title>
-				          <div>
-				            <p class="headline mb-0">eAuction eAuction</p>
-				            <div>eAuction eAuction eAuction eAuction eAuction<br> eAuction eAuction eAuction eAuction</div>
-				          </div>
-				        </v-card-title>
-					</v-card>
-		 		</v-flex>
-		 		<v-flex md3>
-					<v-card color="grey darken-1">
-						<v-card-media src="http://lheventures.com/wp-content/uploads/sites/7/2017/10/Cooke-Auction-Property-pic.png" height="200px">
-						</v-card-media>	
-						<v-card-title primary-title>
-				          <div>
-				            <p class="headline mb-0">eAuction eAuction</p>
-				            <div>eAuction eAuction eAuction eAuction eAuction<br> eAuction eAuction eAuction eAuction</div>
-				          </div>
-				        </v-card-title>
-					</v-card>
-		 		</v-flex>
-		 		<v-flex md3>
-					<v-card color="grey darken-1">
-						<v-card-media src="http://lheventures.com/wp-content/uploads/sites/7/2017/10/Cooke-Auction-Property-pic.png" height="200px">
-						</v-card-media>	
-						<v-card-title primary-title>
-				          <div>
-				            <p class="headline mb-0">eAuction eAuction</p>
-				            <div>eAuction eAuction eAuction eAuction eAuction<br> eAuction eAuction eAuction eAuction</div>
-				          </div>
-				        </v-card-title>
-					</v-card>
-		 		</v-flex>
-		 	</v-layout>
-		</v-container>
-		<v-navigation-drawer 
-			temporary 
-			absolute 
-			v-model="showNav">
-	    	<v-list>
-	    		<v-toolbar class="transparent">
-					<v-toolbar-title>Username Here</v-toolbar-title>
-	    		</v-toolbar>
-	    	</v-list>
-	  	</v-navigation-drawer>
-	</v-layout>
+			<v-flex
+	 			v-for="livestream in livestreams"
+				md3
+				pa-3
+				>
+				<v-card 
+					color="grey darken-1"
+					:hover="true"
+				>
+					<v-card-media 
+						:src="livestream.thumbnail" 
+						height="300px"
+					>
+						<v-layout 
+							row 
+							wrap
+							justify-end
+							pa-2
+						>
+						    <v-icon
+								large
+								color="red lighten-1"
+							>
+								live_tv
+							</v-icon>
+						</v-layout>
+					</v-card-media>	
+					<v-card-title primary-title>
+			          <div>
+			            <p class="headline mb-0">{{livestream.title}}</p>
+			            <div>{{livestream.description}}</div>
+			          </div>
+			        </v-card-title>
+				</v-card>
+			</v-flex>
+ 		</v-layout>
+ 	</v-container>
 </template>
 
 <script>
@@ -165,6 +66,13 @@ export default {
 	data(){
 		return {
 			showNav: false,
+			livestreams: [
+				{
+					title: "eAuction",
+					description: "eAuction",
+					thumbnail: "http://lheventures.com/wp-content/uploads/sites/7/2017/10/Cooke-Auction-Property-pic.png",
+				}
+			],
 		}
 	}
 }
