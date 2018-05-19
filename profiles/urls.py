83bba80c4eaf77002dafd043a45cda76e921fb8d
Gavin <gavincanete@gmail.com>
2018-05-19 16:05:23 +0800

    Changing the path for main page

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('request_profile/', views.request_profile),
-    path('sign_in/', views.view_profile),
-    path('sample/', views.SampleCase.as_view()),
+    path('request_profile_details/', views.request_profile_details),
 ]
