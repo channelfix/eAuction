aae7cf1462d13b2ed096753238c462bebc21adcf
Gavin <gavincanete@gmail.com>
2018-05-25 12:30:32 +0800

    Create an EditPassword class

--- profiles/views.py
+++ profiles/views.py
@@ -30,39 +30,33 @@ class EditProfile(View):
         sent_username = request.POST.get('username', '')
         user = User.objects.get(username=sent_username)
 
-        old_password = request.POST.get('old_password', '')
+        first_name = request.POST.get('first_name', '')
+        last_name = request.POST.get('last_name', '')
+        email = request.POST.get('email', '')
+        biography = request.POST.get('biography', '')
 
-        if(user.check_password(old_password)):
-            new_password = request.POST.get('new_password', '')
-            first_name = request.POST.get('first_name', '')
-            last_name = request.POST.get('last_name', '')
-            email = request.POST.get('email', '')
-            biography = request.POST.get('biography', '')
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.biography = biography
+        user_profile = user.profile
 
-            user.set_password(new_password)
-            user.first_name = first_name
-            user.last_name = last_name
-            user.email = email
-            user.biography = biography
-            user_profile = user.profile
+        if request.FILES:
+            user_profile.avatar = request.FILES['imageFile']
 
-            if request.FILES:
-                user_profile.avatar = request.FILES['imageFile']
+        user_profile.save()
+        user.save()
 
-            user_profile.save()
-            user.save()
-
-            return HttpResponse('Successfully Changed Profile.')
-
-        return HttpResponse('Incorrect Old Password')
+        return HttpResponse('Successfully Changed Profile.')
 
 
-class SaveImage(View):
+class EditPassword(View):
     def post(self, request):
         sent_username = request.POST.get('username', '')
         user = User.objects.get(username=sent_username)
-        profile = user.profile
-        profile.avatar = request.FILES['imageFile']
-        profile.save()
 
-        return HttpResponse('Successfully saved image')
+        if(user.check_password(request.POST.get('old_password', ''))):
+            user.set_password(request.POST.get('new_password', ''))
+            return HttpResponse('Changed Password: You will be redirect to the login page.')
+
+        return HttpResponse('Incorrect Old Password')
