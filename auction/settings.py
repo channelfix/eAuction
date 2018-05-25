543e69949b07b3878502fa6ca475f30af8a7ee71
Gavin <gavincanete@gmail.com>
2018-05-25 16:01:05 +0800

    Change the upload and retrieve path

--- auction/settings.py
+++ auction/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/cache/')
+MEDIA_URL = 'media/cache/'
 
 STATIC_URL = '/static/'
 
