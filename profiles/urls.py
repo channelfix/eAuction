46bdb3012ad2d6f3ea4da63b6e66b29f3c79891f
Gavin <gavincanete@gmail.com>
2018-05-26 19:27:00 +0800

    Adding remove_tag in url pattern

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('request_profile_details/', views.ProfileView.as_view()),
     path('edit_profile_details/', views.EditProfile.as_view()),
     path('edit_password/', views.EditPassword.as_view()),
+    path('remove_tag/', views.TagRemoval.as_view()),
 ]
