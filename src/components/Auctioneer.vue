73506f08f44103bf993b0094f784a35e562ad289
bojoluis21798 <trinity2198@gmail.com>
2018-06-07 18:05:21 +0800

    Add all actions for Auctioneer

--- src/components/Auctioneer.vue
+++ src/components/Auctioneer.vue
@@ -4,39 +4,97 @@
   	>
   		<v-layout 
   			fill-height
-		row
-		wrap
+			row
+			wrap
+			class="box"
+		>
+  			<v-layout 
+  				row 
+  				wrap
 			>
-  			<v-flex 
-			md6
+  				<v-flex 
+					md6
 				>
-			<v-layout 
-				align-center
-				pa-3
-			>
-				<button 
-					class="red"
+					<v-layout 
+						align-center
+						pa-3
+					>
+						<button 
+							class="red"
+						>
+							End Auction
+						</button>	
+					</v-layout>
+  				</v-flex>
+	  			<v-flex 
+				md6
+					>
+					<v-layout
+						align-center
+						pa-3
+					>
+						<button
+							class="green"
+							@click="startAuction"
+						>
+							Start Auction
+						</button>	
+					</v-layout>
+	  			</v-flex>
+	  			<v-flex 
+					md4
 				>
-					End Auction
-				</button>	
-			</v-layout>
-  			</v-flex>
-  			<v-flex 
-			md6
+					<v-layout 
+						align-center
+						pa-3
+					>
+						<button 
+							class="red"
+						>
+							Close auction for current item
+						</button>	
+					</v-layout>
+  				</v-flex>
+  				<v-flex 
+					md5
 				>
-			<v-layout
-				align-center
-				pa-3
-			>
-				<button
-					class="green"
-					@click="startAuction"
+					<v-layout 
+						align-center
+						justify-center
+						pa-3
+					>
+						<v-text-field
+							label="Bid Increase"
+						>
+							
+						</v-text-field>
+						<button class="halfsize green">Increase</button>
+					</v-layout>
+  				</v-flex>
+  				<v-flex 
+					md3
 				>
-					Start Auction
-				</button>	
-			</v-layout>
-  			</v-flex>
-  		</v-layout> 
+					<v-layout 
+						align-center
+						justify-center
+						pa-3
+					>
+						<button class="green">Move to next item</button>
+					</v-layout>
+  				</v-flex>
+  				<v-flex 
+					md3
+				>
+					<v-layout 
+						align-center
+						justify-center
+						pa-3
+					>
+						<button class="green">Open auction current item</button>
+					</v-layout>
+  				</v-flex>
+	  		</v-layout>
+  		</v-layout>
   	</v-flex>
 </template>
 
@@ -107,5 +165,9 @@ export default {
 	button {
 		height: 5em;
 		width: 100%;
+	}
+
+	.halfsize {
+		width: 50%;
 	}
 </style>
