eb7bed91c923710cd5dcc40c46798ed09012be67
Zairiel Sarausad <zairiel243@gmail.com>
2018-06-09 18:46:51 +0800

    Add checkers for form inputs

--- src/components/VRegisterForm.vue
+++ src/components/VRegisterForm.vue
@@ -1,48 +1,72 @@
 <template>
-	<v-form class="form">
+	<v-form class="form" ref="form" v-model="valid" lazy-validation>
+		<v-alert v-model="alert" type="error" transition="slide-y-transition" dismissible>
+		    	Please fill in form promptly
+		    </v-alert>
 		<v-layout row wrap justify-space-between>
 			<v-flex lg5>
 				<v-text-field
-				  name="fname"
 				  label="Firstname"
 				  id="fn"
 				  v-model="user.fname"
+				  required
+				  :counter="15"
+				  :rules="fnameRules"
 				></v-text-field>
 			</v-flex>
 			<v-flex lg5>
 				<v-text-field
-				  name="lname"
 				  label="Last Name"
 				  id="ln"
 				  v-model="user.lname"
+				  required
+				  :counter="15"
+				  :rules="lnameRules"
 				></v-text-field>
 			</v-flex>
 		</v-layout>
 
 		<v-text-field
-		  name="user"
 		  label="Username"
 		  id="un"
 		  v-model="user.name"
+		  required
+		  :counter="20"
+		  :rules="unameRules"
 		></v-text-field>
 		<v-text-field
-		  name="email"
 		  label="Email"
 		  id="em"
 		  v-model="user.email"
+		  required
+		  :rules="emailRules"
 		></v-text-field>
 		<v-text-field
+		  required
 		  name="pass"
 		  label="Password"
 		  id="ps"
+		  hint="At least 8 characters"
 		  v-model="user.password"
+		  :append-icon="iconVis ? 'visibility' : 'visibility_off'"
+		  :append-icon-cb="() => (iconVis = !iconVis)"
+		  :type="iconVis ? 'password': 'text'"
+		  min="8"
 		></v-text-field>
 		<v-text-field
+		  :error="err"
+		  required
+		  v-model="user.cpassword"
 		  name="confirmpass"
 		  label="Confirm Password"
+		  :append-icon="iconVis ? 'visibility' : 'visibility_off'"
+		  :append-icon-cb="() => (iconVis = !iconVis)"
+		  :type="iconVis ? 'password': 'text'"
+		  min="8"
 		  id="cps"
 		></v-text-field>
-		<v-btn @click="register">Submit</v-btn>
+		<v-btn :disabled="!valid" @click="register">Submit</v-btn>
+		<v-btn @click="clear()">clear</v-btn>
 	</v-form>
 </template>
 
@@ -56,18 +80,41 @@
 				user: {
 					name: "",
 					password:"",
+					cpassword:"",
 					fname:"",
 					lname:"",
 					email:"",
 				},
 				visible: false,
+				valid: false,
+				iconVis: false,
+				alert: false,
+				checkVar: true,
+				fnameRules: [
+					v => !!v || 'First name is required',
+      				v => (v && v.length <= 15) || 'First name must be less than 15 characters',
+      				v => /^\w+/.test(v) || "Field shouldn't start with symbols"
+				],
+				lnameRules: [
+					v => !!v || 'Last name is required',
+      				v => (v && v.length <= 15) || 'Last name must be less than 15 characters',
+      				v => /^\w+/.test(v) || "Field shouldn't start with symbols"
+				],
+				unameRules: [
+					v => !!v || 'Username is required',
+      				v => (v && v.length <= 20) || 'Username must be less than 20 characters',
+      				v => /^\w+/.test(v) || "Field shouldn't start with symbols"
+				],
+				emailRules: [
+			      	v => !!v || 'E-mail is required',
+			      	v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+			    ],
 			}
 		},
 		methods: {
-			register: function(){
+			register(){
 				let request = new Request();
 				let formData = new FormData();
-
 
 				formData.set('username', this.user.name);
 				formData.set('password', this.user.password);
@@ -75,12 +122,38 @@
 				formData.set('firstname', this.user.fname);
 				formData.set('lastname', this.user.lname);
 
+				for(let data of formData.values()){
+					if(data == ""){
+						this.alert = true;
+						return
+					}
+				}
+
 				request.post('/register/', formData, 
-					function(response) {
+					(response)=> {
 						// process response from server
 					}
 				);	
-			}
+			},
+			clear() {
+				this.user.name = "";
+				this.user.password = "";
+				this.user.cpassword = "";
+				this.user.fname = "";
+				this.user.lname = "";
+				this.user.email = "";
+				this.valid = false;
+			},
+		},
+		computed:{
+			err(){
+				if(this.user.password == this.user.cpassword){
+					return false;
+				} else {
+					this.valid = false;
+					return true;
+				}
+			},
 		}
 	}
 </script>
