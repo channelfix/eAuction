d1f5452d7a40b94b29cd641b1a016d3732f56ad8
Gavin Mcrey Canete <gavincanete@gmail.com>
2018-06-11 16:55:04 +0800

    added path for creating and retrieving the product

--- profiles/urls.py
+++ profiles/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('subscribe/', views.Subscribe.as_view()),
     path('update_credits/', views.UpdateCredits.as_view()),
     path('tag_list/', views.TagListView.as_view()),
-    path('create_product/', views.ProductCreatedView.as_view())
+    path('create_product/', views.ProductCreatedView.as_view()),
+    path('retrieve_product/', views.RetrieveProductView.as_view())
 ]
