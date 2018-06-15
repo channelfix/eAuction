b8ac3ab700810b034d2853ecb36d6227a94c6d92
Gavin Mcrey Canete <gavincanete@gmail.com>
2018-06-15 08:03:39 +0800

    No changes

--- auction/settings.py
+++ auction/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'register',
     'profiles',
     'livestream',
+    'phonenumber_field',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
